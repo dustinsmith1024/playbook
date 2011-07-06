3add715f928b50f59c83abfc2ff75eb76265f265
Dustin Smith <dustinsmith1024@hotmail.com>
2011-07-05 21:41:18 -0500

    adding in selection tools

--- public/javascripts/playbook.js
+++ public/javascripts/playbook.js
@@ -104,39 +104,111 @@ function addO(x, y) {
 	//  invalidate();
 }
 
-function drawX(a, b) {
-  a = a + 5; //these make the X start in the middle
-  b = b + 5;
-  a2 = a - 10;
-  b2 = b - 10;
-  $("canvas").drawLine({
-    strokeStyle: "#000",
-    strokeWidth: 3,
-    strokeCap: "round",
-    strokeJoin: "round",
-    x1: a, y1: b,
-    x2: a2, y2: b2
-  });
+var canvas;
+var ctx;
+var WIDTH;
+var HEIGHT;
+var INTERVAL = 20;  // how often, in milliseconds, we check to see if a redraw is needed
 
-  $("canvas").drawLine({
-    strokeStyle: "#000",
-    strokeWidth: 3,
-    strokeCap: "round",
-    strokeJoin: "round",
-    x1: a, y1: b2,
-    x2: a2, y2: b,
-    fromCenter: true
-  });
+var isDrag = false;
+var mx, my; // mouse coordinates
+
+// when set to true, the canvas will redraw everything
+// invalidate() just sets this to false right now
+// we want to call invalidate() whenever we make a change
+var canvasValid = false;
+
+// The node (if any) being selected.
+// If in the future we want to select multiple objects, this will get turned into an array
+var mySel; 
+
+// The selection color and width. Right now we have a red selection with a small width
+var mySelColor = '#CC0000';
+var mySelWidth = 2;
+
+// we use a fake canvas to draw individual shapes for selection testing
+var ghostcanvas;
+var gctx; // fake canvas context
+
+// since we can drag from anywhere in a node
+// instead of just its x/y corner, we need to save
+// the offset of the mouse when we start dragging.
+var offsetx, offsety;
+
+// Padding and border style widths for mouse offsets
+var stylePaddingLeft, stylePaddingTop, styleBorderLeft, styleBorderTop;
+
+// initialize our canvas, add a ghost canvas, set draw loop
+// then add everything we want to intially exist on the canvas
+function init() {
+	$canvas = $('canvas');
+	HEIGHT = $canvas.height();
+	WIDTH = $canvas.width();
+	ctx = $canvas.loadCanvas('2d');
+	$ghostcanvas = $('canvas');
+	$ghostcanvas.height = HEIGHT;
+	$ghostcanvas.width = WIDTH;
+	gctx = $ghostcanvas.loadCanvas("2d");
+	
+	//fixes a problem where double clicking causes text to get selected on the canvas
+	$canvas.select(function () { return false; });
+	
+	// fixes mouse co-ordinate problems when there's a border or padding
+	// see getMouse for more detail
+	if (document.defaultView && document.defaultView.getComputedStyle) {
+		stylePaddingLeft = parseInt(document.defaultView.getComputedStyle(canvas, null)['paddingLeft'], 10)      || 0;
+		stylePaddingTop  = parseInt(document.defaultView.getComputedStyle(canvas, null)['paddingTop'], 10)       || 0;
+		styleBorderLeft  = parseInt(document.defaultView.getComputedStyle(canvas, null)['borderLeftWidth'], 10)  || 0;
+		styleBorderTop   = parseInt(document.defaultView.getComputedStyle(canvas, null)['borderTopWidth'], 10)   || 0;
+	}
+	
+	// make draw() fire every INTERVAL milliseconds.
+	setInterval(draw, INTERVAL);
+	
+	// add our events. Up and down are for dragging,
+	// double click is for making new boxes
+	$canvas.mousedown(function(){
+				myDown;
+	}).mouseup(function(){
+				myUp;
+	}).dblclick(function(){
+				myDblClick;
+	});
+	
+	// add custom initialization here:
+	//add court or draw original stuff
+	// add an orange rectangle
+	addRect(200, 200, 40, 40, '#FFC02B');
+	
+	// add a smaller blue rectangle
+	addRect(25, 90, 25, 25, '#2BB8FF');
 }
 
-function drawO(a,b) {
-
-$("canvas").drawEllipse({
-  //fillStyle: "#000",
-  strokeStyle: "#000",
-  x: a, y: b,
-  width: 15, height: 15
-})
-
-
+// While draw is called as often as the INTERVAL variable demands,
+// It only ever does something if the canvas gets invalidated by our code
+function draw() {
+	if (canvasValid == false) {
+		$canvas.clearCanvas();
+		
+		// Add stuff you want drawn in the background all the time here
+		
+		// draw all boxes
+		var l = team.length;
+		for (var i = 0; i < l; i++) {
+			addX(boxes[i].x, boxes[i].y);
+		}
+		
+		// draw selection
+		// right now this is just a stroke along the edge of the selected box
+		if (mySel != null) {
+			ctx.strokeStyle = mySelColor;
+			ctx.lineWidth = mySelWidth;
+			ctx.strokeRect(mySel.x,mySel.y,mySel.w,mySel.h);
+		}
+		
+		// Add stuff you want drawn on top all the time here
+		
+		
+		canvasValid = true;
+	}
 }
